36e7598854a8a354b2ea074093dfa9ec89e9e6c5
MillionBenjain <617572667@qq.com>
2020-10-19 15:00:04 +0800

    week 4

--- code/competition_1_tianchi/week_4_xgboost/logistic_xgboost.py
+++ code/competition_1_tianchi/week_4_xgboost/logistic_xgboost.py
@@ -1,26 +1,34 @@
-# 第三周周报
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-> 本次实践完整代码见[../code/competition_1_tianchi/week_3_non_numerical](../code/competition_1_tianchi/week_3_non_numerical)
+data_train =pd.read_csv('../train.csv')
+data_test_a = pd.read_csv('../testA.csv')
 
-- [第三周周报](#第三周周报)
-  - [一、特征工程](#一特征工程)
-    - [1. `grade` 数值化](#1-grade-数值化)
-    - [2. `subGrade` 数值化](#2-subgrade-数值化)
-    - [3. `employmentLength` 数值化](#3-employmentlength-数值化)
-    - [4. `issueDate` 数值化](#4-issuedate-数值化)
-    - [5. `earliesCreditLine` 数值化](#5-earliescreditline-数值化)
-  - [二、模型选择](#二模型选择)
-    - [1. Logistic Regression](#1-logistic-regression)
+#所有数值字段
+numerical_fea = list(data_train.select_dtypes(exclude=['object']).columns)
+#所有非数值字段
+category_fea = list(filter(lambda x: x not in numerical_fea,list(data_train.columns)))
+#标签字段为：isDefault
+label = 'isDefault'
+#去除数值字段中的标签字段
+numerical_fea.remove(label)
+#去除数值字段中的id字段
+numerical_fea.remove("id")
 
-## 一、特征工程
+#按照中位数填充数值型特征
+data_train[numerical_fea] = data_train[numerical_fea].fillna(data_train[numerical_fea].median())
+data_test_a[numerical_fea] = data_test_a[numerical_fea].fillna(data_train[numerical_fea].median())
+#按照众数填充类别型特征
+data_train[category_fea] = data_train[category_fea].fillna(data_train[category_fea].mode())
+data_test_a[category_fea] = data_test_a[category_fea].fillna(data_train[category_fea].mode())
 
-在第二周的特征工程中，进行了**数据选择**和**缺失值处理**。详见[第二周周报](./week_report_2.md)。
+#print(data_train[numerical_fea])
+print(data_train.isnull().sum())
 
-本周特征工程，对非数值数据进行数值化处理，以便加入训练。具体的数值化方法均为自己所想，可能有不完善之处。
+print(category_fea)
 
-### 1. `grade` 数值化
 
-```python
 # grade 映射
 # 检视grade下有哪些值，放入列表并排序
 distinct_grade_value = list(data_train["grade"].unique())
@@ -41,11 +49,7 @@ data_test_a[grade_number_column_name] = data_test_a["grade"].apply(grade_map_to_
 # grade_number_column_name是数值型字段，将grade_number_column_name加入numerical_fea
 numerical_fea.append(grade_number_column_name)
 
-```
 
-### 2. `subGrade` 数值化
-
-```python
 # subGrade 映射
 # 检视subGrade下有哪些值，放入列表并排序
 distinct_subGrade_value = list(data_train["subGrade"].unique())
@@ -63,11 +67,6 @@ data_test_a[subGrade_number_column_name] = data_test_a["subGrade"].apply(subGrad
 # subGrade_number_column_name是数值型字段，将subGrade_number_column_name加入numerical_fea
 numerical_fea.append(subGrade_number_column_name)
 
-```
-
-### 3. `employmentLength` 数值化
-
-```python
 
 # employmentLength映射
 # 检视employmentLength下有哪些值，放入列表并排序
@@ -92,21 +91,12 @@ data_train[employmentLength_number_column_name] = data_train["employmentLength"]
 data_test_a[employmentLength_number_column_name] = data_test_a["employmentLength"].apply(employmentLength_map_to_number)
 # employmentLength_number_column_name是数值型字段，将employmentLength_number_column_name加入numerical_fea
 numerical_fea.append(employmentLength_number_column_name)
-
-```
-
-**注意**：数据中有nan，用中位数填补之。
-
-```python
 # 数据中有nan，用中位数填补之
 data_train[employmentLength_number_column_name] = data_train[employmentLength_number_column_name].fillna(data_train[employmentLength_number_column_name].median())
 data_test_a[employmentLength_number_column_name] = data_test_a[employmentLength_number_column_name].fillna(data_train[employmentLength_number_column_name].median())
 
-```
 
-### 4. `issueDate` 数值化
 
-```python
 # issueDate映射
 # 检视issueDate下有哪些值，放入列表并排序
 distinct_issueDate_value = list(data_train["issueDate"].unique())
@@ -130,11 +120,7 @@ data_test_a[issueDate_number_column_name] = data_test_a["issueDate"].apply(issue
 # issueDate_number_column_name是数值型字段，将issueDate_number_column_name加入numerical_fea
 numerical_fea.append(issueDate_number_column_name)
 
-```
 
-### 5. `earliesCreditLine` 数值化
-
-```python
 
 # earliesCreditLine映射
 # 检视earliesCreditLine下有哪些值，放入列表并排序
@@ -162,22 +148,38 @@ data_train[earliesCreditLine_number_column_name] = data_train["earliesCreditLine
 data_test_a[earliesCreditLine_number_column_name] = data_test_a["earliesCreditLine"].apply(earliesCreditLine_map_to_number)
 # earliesCreditLine_number_column_name是数值型字段，将earliesCreditLine_number_column_name加入numerical_fea
 numerical_fea.append(earliesCreditLine_number_column_name)
-print(data_train[earliesCreditLine_number_column_name])
-
-```
 
 
-## 二、模型选择
+train_x = np.asarray(data_train[numerical_fea])
+train_y = np.asarray(data_train[label])
 
-### 1. Logistic Regression
+test_x = np.asarray(data_test_a[numerical_fea])
+#test_y = np.asarray(data_test_a[label])
 
-本次选择模型与第二周所选模型相同。与第二周不同的是，训练与预测时加入了数值化后的非数值数据。
+import xgboost as xgb
+# 根据训练数据生成xgboost可用的数据集DMatrix
+dtrain = xgb.DMatrix(train_x, label = train_y)
+# 确定训练参数
+param = {'max_depth': 6, 'eta': 0.3, 'objective':'binary:logistic' }
+num_round = 8
+# 进行训练
+bst = xgb.train(param, dtrain, num_round)
+# 保存训练得到的模型
+bst.save_model('0001.model')
+# 根据测试数据生成xgboost可用的数据集DMatrix
+dtest = xgb.DMatrix(test_x)
+# 用训练得到的模型进行预测
+result = bst.predict(dtest)
 
-本次成绩：
+print(result)
+result = np.asarray(result)
 
-![](./week_report_3_images/logistic_score.png)
+#result_dataframe = pd.DataFrame(data = {"id": data_test_a["id"], "isDefault": [i[1] for i in list(result)]})
+result_dataframe = pd.DataFrame(data = {"id": data_test_a["id"], "isDefault": ["{:f}".format(i) for i in list(result)]})
+print(result_dataframe)
 
-上次不使用非数值化数据的逻辑回归分数为：0.6938。可见使用非数值化数据提升了准确率。
+result_dataframe.to_csv("result_xgboost.csv", index=False)
 
+xgb.plot_importance(bst)
 
 
---
+++ week_report/week_report_4.md
@@ -0,0 +1,88 @@
+# 第四周周报
+
+> 本次实践完整代码见[../code/competition_1_tianchi/week_3_xgboost](../code/competition_1_tianchi/week_3_xgboost)
+
+- [第四周周报](#第四周周报)
+  - [一、特征工程](#一特征工程)
+  - [二、模型选择](#二模型选择)
+    - [1. Gradient Boosted Decison Trees](#1-gradient-boosted-decison-trees)
+      - [a. GBDT与XGBoost介绍](#a-gbdt与xgboost介绍)
+      - [b. MacOS中安装XGBoost (Python)](#b-macos中安装xgboost-python)
+      - [c. XGBoost参数介绍](#c-xgboost参数介绍)
+      - [d. XGBoost的使用](#d-xgboost的使用)
+      - [e. 本次成绩](#e-本次成绩)
+
+## 一、特征工程
+
+在第二周的特征工程中，进行了**数据选择**和**缺失值处理**。详见[第二周周报](./week_report_2.md)。
+
+在第三周的特征工程中，进行了**非数值数据转化为数值数据**。详见[第三周周报](./week_report_3.md)。
+
+本周数据沿用前两周特征工程处理出的数据进行训练。
+
+
+## 二、模型选择
+
+### 1. Gradient Boosted Decison Trees
+
+#### a. GBDT与XGBoost介绍
+Gradient Boosted Decison Trees即GBDT，梯度提升树，又可简称为GBT。GBDT每轮迭代的目的是找到决策树，使样本的损失尽量变得更小。
+
+本次代码中使用的是XGBoost中的GBDT模型。XGBoost是一个开源库，实现了梯度提升树模型。
+
+#### b. MacOS中安装XGBoost (Python)
+
+```bash
+brew install libomp
+
+pip3 install xgboost
+```
+
+在其他环境安装XGBoost的方法可见[官方英文文档-Installation Guide](https://xgboost.readthedocs.io/en/latest/build.html)。
+
+#### c. XGBoost参数介绍
+
+全部参数介绍可见[官方英文文档-XGBoost Parameters](https://xgboost.readthedocs.io/en/latest/parameter.html)，在此介绍本次代码中自定义使用的参数（已自行翻译为中文）。
+
+`eta`：即学习率`learning_rate`，默认为0.3。范围[0, 1]
+
+`max_depth`：树最大深度，默认为6。此值越大，模型越复杂，越容易过拟合。范围[0, ∞] （只有使用 `lossguided` 生长策略，`tree_method` 设为 `hist` 时才能为0）
+
+`objective`：确定学习任务及对应的学习目标，默认为 `reg:squarederror`（均方误差回归）。本次代码设为 `binary:logistic` ，即二分类逻辑回归，输出概率。
+
+`num_round`：提升的轮数。
+
+#### d. XGBoost的使用
+
+本次代码中使用XGBoost：
+
+```python
+import xgboost as xgb
+# 根据训练数据生成xgboost可用的数据集DMatrix
+dtrain = xgb.DMatrix(train_x, label = train_y)
+# 确定训练参数
+param = {'max_depth': 6, 'eta': 0.3, 'objective':'binary:logistic' }
+num_round = 8
+# 进行训练
+bst = xgb.train(param, dtrain, num_round)
+# 保存训练得到的模型
+bst.save_model('0001.model')
+# 根据测试数据生成xgboost可用的数据集DMatrix
+dtest = xgb.DMatrix(test_x)
+# 用训练得到的模型进行预测
+result = bst.predict(dtest)
+```
+
+#### e. 本次成绩
+
+
+
+
+
+
+
+
+
+
+
+
